b6ef0d18036f45db5754c6765aaec012b9391d29
Ruohong Liu <ruohong.leo7@gmail.com>
2014-11-10 23:31:23 -0500

    Design with bugs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,22 @@
 /* Submit the form on the same page */
 
 $(document).ready(function(){
-	$('.action').on('submit', '.csubmit', function(){
-		var items = $('edititem').val();
-		$('a-item-description').text(items);
+	$('.csubmit').on('click', function(){
+		var items = $('.edititem').val();
+		var prices = $('.editprice').val();
+		var quantities = $('.editquantity').val();
+		var total =$('<li><div class="action-image"><a href="#" class="delete"><img src="assets/img/delete.png" /></a><a href="#" class="check"><img src="assets/img/check.png" /></a></div> <div class="item-data"><span class="a-item-description">' + items + '</span></div><div class="price-data"><span class="a-price"> $' + prices + '</span></div><div class="quantity-data"><span class="a-quantity">' + quantities + '</span></div></li>');
+		$('#list').prepend(total);
+		total.hide().slideDown(600);
+	});
+
+	$('.check').on('click', function(){
+		event.preventDefault();
+		$(this).closest('li').css("background-color", "red");
+	});
+
+	$('.delete').on('click', function(){
+		event.preventDefault();
+		$(this).closest('li').remove();
 	});
 });
