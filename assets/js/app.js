79d559b0422d395d44852b6db1c151fe73df1ddc
James Van Dyke <jamesvd@gmail.com>
2014-11-11 20:48:07 -0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,8 @@
 /* Submit the form on the same page */
+var shoppingPage = {}
 
-$(document).ready(function(){
-	$('.csubmit').on('click', function(){
-		var items = $('.edititem').val();
-		var prices = $('.editprice').val();
-		var quantities = $('.editquantity').val();
-		var total =$('<li><div class="action-image"><a href="#" class="delete"><img src="assets/img/delete.png" /></a><a href="#" class="check"><img src="assets/img/check.png" /></a></div> <div class="item-data"><span class="a-item-description">' + items + '</span></div><div class="price-data"><span class="a-price"> $' + prices + '</span></div><div class="quantity-data"><span class="a-quantity">' + quantities + '</span></div></li>');
-		$('#list').prepend(total);
-		total.hide().slideDown(600);
-	});
+shoppingPage.init = function() {
+	shoppingPage.initSubmit()
 
 	$('.check').on('click', function(){
 		event.preventDefault();
@@ -19,4 +13,17 @@ $(document).ready(function(){
 		event.preventDefault();
 		$(this).closest('li').remove();
 	});
-});
+}
+
+shoppingPage.initSubmit = function() {
+	$('.csubmit').on('click', function(){
+		var items = $('.edititem').val();
+		var prices = $('.editprice').val();
+		var quantities = $('.editquantity').val();
+		var total =$('<li><div class="action-image"><a href="#" class="delete"><img src="assets/img/delete.png" /></a><a href="#" class="check"><img src="assets/img/check.png" /></a></div> <div class="item-data"><span class="a-item-description">' + items + '</span></div><div class="price-data"><span class="a-price"> $' + prices + '</span></div><div class="quantity-data"><span class="a-quantity">' + quantities + '</span></div></li>');
+		$('#list').prepend(total);
+		total.hide().slideDown(600);
+	});
+}
+
+$(document).ready(shoppingPage.init;
